bc9eebef2be4bb392ab087b7df9ffb0116368e64
Wayne Boka <boka.wayne@gmail.com>
2017-01-04 22:06:18 -0500

    - Get lines for selected files - Adding Linux keymap - Open the file if only one selection is found

--- OpenHighlightedFile.py
+++ OpenHighlightedFile.py
@@ -5,15 +5,21 @@ import datetime
 # Simple plugin that attempts to open the highlighted text as a file in your project directory
 class OpenHighlightedFileCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		# Grab all of the highlighted lines
-		highlighted_file = ''.join(self.view.substr(l) for l in self.view.sel())
+		lines = []
+
+		for selectedRegion in self.view.sel():
+			selectedLines = self.view.lines(selectedRegion)
+
+			for line in selectedLines:
+				 lines.append(self.view.substr(line))
 
 		# Do we have at least one line to work with?
-		if len(highlighted_file) > 0:
-			# Inform the user what file we are attempting to open
-			print("OpenHighlightedFile: Opening {}".format(highlighted_file))
-			# Open the file
-			self.view.window().open_file(highlighted_file)
+		if len(lines) == 1:
+			# Only one, open the file or open a new tab to create it
+			self.view.window().open_file(lines[0])
+		elif len(lines) > 0:
+			# Open a quick panel containing the selected file names
+			self.view.window().show_quick_panel(lines, lambda i: self.view.window().open_file(lines[i]))
 		else:
 			# Show the console
 			self.view.window().run_command("show_panel", { "panel": "console", "toggle": False })
